d6a65f391ef4441530405dbb3045c6a2ad5de69c
zinebMachrouh <zinebmac49@gmail.com>
2024-12-17 16:05:25 +0100

    initializing spring boot project

---
+++ src/main/java/org/example/ayziwai/AyZiWaiApplication.java
@@ -0,0 +1,13 @@
+package org.example.ayziwai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyZiWaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyZiWaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ayziwai/AyZiWaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ayziwai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyZiWaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
